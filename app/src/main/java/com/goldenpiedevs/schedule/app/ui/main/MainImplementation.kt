a1bd2b6002956ae103f5f5436fbb318206957cd9
a.artemov <a.artemov@kernel.ua>
2018-08-08 10:31:52 +0300

    Show week name in toolbar

--- app/src/main/java/com/goldenpiedevs/schedule/app/ui/main/MainImplementation.kt
+++ app/src/main/java/com/goldenpiedevs/schedule/app/ui/main/MainImplementation.kt
@@ -2,13 +2,21 @@ package com.goldenpiedevs.schedule.app.ui.main
 
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import com.goldenpiedevs.schedule.app.R
 import com.goldenpiedevs.schedule.app.R.id.container
+import com.goldenpiedevs.schedule.app.core.api.lessons.LessonsManager
+import com.goldenpiedevs.schedule.app.core.ext.currentWeek
 import com.goldenpiedevs.schedule.app.core.ext.todayName
 import com.goldenpiedevs.schedule.app.core.utils.AppPreference
 import com.goldenpiedevs.schedule.app.ui.base.BasePresenterImpl
 import com.goldenpiedevs.schedule.app.ui.timetable.TimeTableFragment
+import kotlinx.coroutines.experimental.launch
+import javax.inject.Inject
 
 class MainImplementation : BasePresenterImpl<MainView>(), MainPresenter {
+
+    @Inject
+    lateinit var lessonsManager: LessonsManager
 
     private lateinit var supportFragmentManager: FragmentManager
 
@@ -31,10 +39,10 @@ class MainImplementation : BasePresenterImpl<MainView>(), MainPresenter {
             //String immutable, seems it does not like apply
             var title = todayName
             title = title.substring(0, 1).toUpperCase() + title.substring(1)
+            title = "$title, ${currentWeek + 1} ${view.getContext().getString(R.string.week)}"
 
             setActivityTitle(title)
         }
-
     }
 
     override fun onMapClick() {
@@ -60,4 +68,11 @@ class MainImplementation : BasePresenterImpl<MainView>(), MainPresenter {
             (view.getContext() as AppCompatActivity).finish()
         }
     }
+
+    override fun loadTimeTable() {
+        launch {
+            lessonsManager.loadTimeTable(AppPreference.groupId)
+        }
+    }
+
 }

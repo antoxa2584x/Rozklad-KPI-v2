22dd3d6d4e5cba5d481aa6d5c4447037dfee9bed
a.artemov <a.artemov@kernel.ua>
2018-08-13 11:41:23 +0300

    Provided Fabric and LeakCanary

--- app/src/main/java/com/goldenpiedevs/schedule/app/ScheduleApplication.kt
+++ app/src/main/java/com/goldenpiedevs/schedule/app/ScheduleApplication.kt
@@ -2,7 +2,10 @@ package com.goldenpiedevs.schedule.app
 
 import android.support.multidex.MultiDexApplication
 import com.chibatching.kotpref.Kotpref
+import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
+import com.squareup.leakcanary.LeakCanary
+import io.fabric.sdk.android.Fabric
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -10,6 +13,14 @@ class ScheduleApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+
+        LeakCanary.install(this)
+        Fabric.with(this, Crashlytics())
+
         Kotpref.init(this)
         AndroidThreeTen.init(this)
         Realm.init(this)
